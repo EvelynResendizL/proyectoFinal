150fcded7ef7f11758bf7ab65a95e0f5394dda9b
EvelynResendizL <evelynresendiz888@gmail.com>
2025-05-06 13:52:17 -0600

    Update carga_inicial.sql

--- Scripts/carga_inicial.sql
+++ Scripts/carga_inicial.sql
@@ -3,13 +3,16 @@
 --Es importante que este comando se ejecute desde la terminal de PostgreSQL (psql)
 
 --Código para crear la tabla con la información que viene del CSV
+DROP TABLE IF EXISTS afluencia_metro;
+
 CREATE TABLE afluencia_metro (
-    fecha DATE,            
-    anio INTEGER,          
-    mes TEXT,              
-    linea TEXT,            
-    estacion TEXT,         
-    afluencia INTEGER      
+    id SERIAL PRIMARY KEY,           
+    fecha DATE NOT NULL,             
+    anio INTEGER NOT NULL,          
+    mes VARCHAR(15) NOT NULL,        
+    linea VARCHAR(50) NOT NULL,      
+    estacion VARCHAR(100) NOT NULL,  
+    afluencia INTEGER NOT NULL       
 );
 
 --Código para cargar los datos desde el CSV 
