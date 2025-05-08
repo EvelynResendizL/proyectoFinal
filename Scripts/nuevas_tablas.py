0745d302c69e48c60d42cafa7ad4805517d0fdc2
EvelynResendizL <evelynresendiz888@gmail.com>
2025-05-08 12:17:18 -0600

    Add files via upload

---
+++ Scripts/nuevas_tablas.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: evely
+"""
+import pandas as pd
+
+# Cargar archivo limpio
+file_path = "C:/Users/evely/Downloads/ProyectoFinalBD/afluencia_utf8.csv"
+df = pd.read_csv(file_path)
+
+# Corregir columna con BOM si aplica
+df.columns = [col.strip().replace('ï»¿', '') for col in df.columns]
+
+# Convertir fecha a datetime
+df['fecha'] = pd.to_datetime(df['fecha'], dayfirst=True, errors='coerce')
+
+# Añadir columnas derivadas
+df['dia_semana'] = df['fecha'].dt.day_name()
+df['tipo_dia'] = df['dia_semana'].apply(lambda d: 'Fin de semana' if d in ['Saturday', 'Sunday'] else 'Laboral')
+df['semana_del_anio'] = ((df['fecha'] - pd.to_datetime(df['fecha'].dt.year.astype(str) + '-01-01')).dt.days // 7) + 1
+
+# Crear columna vacía (temporal) para zona
+df['zona'] = 'otra'
+
+# Guardar el archivo enriquecido
+output_path ="C:/Users/evely/Downloads/ProyectoFinalBD/afluencia_utf8.csv"
+df.to_csv(output_path, index=False)
+
+print(f"Archivo enriquecido guardado en: {output_path}")
+
